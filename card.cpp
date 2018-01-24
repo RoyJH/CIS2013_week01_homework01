a5ea4bd8c1b263cd5c936929aa8891df11bd414b
RoyJH <RoyJH@Github.com>
2018-01-24 14:23:23 -0600

    Initial commit

---
+++ card.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+	char something;
+	cout<< "0xxx|==========>\t OSU-OKC \t<==========|xxx0\n"
+	<< "\t\tRoy J Harris III (Trey)\n"
+	<< "\t    Swimming Pool Service Technician\n" 
+	<< "Computer Information Systems - Computer Game Programming\n"
+	<<endl;
+	cout<< "\t1006 W Ridgecrest Way\tPhone: (405) 613-2591\n"
+	<< "\tMustang, OK 73064\tEmail: royjh@okstate.edu\n"
+	<< "\t\t\t\tGitHub: RoyJH\n"
+	<< "\t\t\t\tSteam: Zithlos\n"
+	<< "0xxx|==========>\t   C++   \t<==========|xxx0\n"
+	<<endl;
+	system ("PAUSE");
+	return 0;
+}
